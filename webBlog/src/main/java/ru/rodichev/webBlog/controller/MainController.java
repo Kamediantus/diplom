58a76545dbb068818d0eae72024f4788f22801e9
Kamediantus <liprikent@yandex.ru>
2021-04-03 14:27:51 +0300

    remove "note" link from header, note -> main

--- webBlog/src/main/java/ru/rodichev/webBlog/controller/MainController.java
+++ webBlog/src/main/java/ru/rodichev/webBlog/controller/MainController.java
@@ -1,6 +1,7 @@
 package ru.rodichev.webBlog.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.rodichev.webBlog.entity.Notes;
+import ru.rodichev.webBlog.repo.NotesRepository;
 import ru.rodichev.webBlog.repo.UserRepository;
 import ru.rodichev.webBlog.service.UserService;
 
@@ -20,16 +23,19 @@ import javax.servlet.http.HttpServletResponse;
 public class MainController {
     private UserRepository userRepository;
 
+    @Autowired
+    private NotesRepository notesRepository;
+
     @GetMapping("/")
     public String homepage(Model model){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth instanceof AnonymousAuthenticationToken)) {
-            String roles = auth.getDetails().toString();
             String currentUsername = auth.getName();
             model.addAttribute("user", currentUsername);
-            model.addAttribute("roles", roles);
         }
-        return "homePage";
+        Iterable<Notes> notes = notesRepository.reverseFindAll();
+        model.addAttribute("notes", notes);
+        return "mainNotes";
     }
     @GetMapping("/contacts")
     public String contacts(Model model){
